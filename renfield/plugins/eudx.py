8e0b66bc897cce198a5a7b9ef531db65ec22f50e
mbridak <michael.bridak@gmail.com>
2026-06-13 16:47:05 -0700

    @mbridak Add EUDX

---
+++ renfield/plugins/eudx.py
@@ -0,0 +1,304 @@
+""" """
+
+import datetime
+from pathlib import Path
+
+
+from not1mm.lib.plugin_common import gen_adif, imp_adif, get_points, online_score_xml
+from not1mm.lib.version import __version__
+
+assert imp_adif
+assert online_score_xml
+
+EXCHANGE_HINT = "ITU or EU Union Region"
+SOAPBOX_HINT = """"""
+
+name = "EUDXC"
+mode = "BOTH"  # CW SSB BOTH RTTY
+cabrillo_name = "EUDXC"
+
+
+# 1 once per contest, 2 work each band, 3 each band/mode, 4 no dupe checking
+dupe_type = 3
+
+
+def show_mults(self, rtc=None):
+    """Return display string for mults"""
+
+    """
+    • European Union Region: A multiplier of one (1) for each different European Union Region contacted on each band;
+    • Country: A multiplier of one (1) for each different country contacted on each band.
+    """
+
+    eud = 0
+    country_prefix = 0
+
+    sql = (
+        "select count(DISTINCT(NR || ':' || Mode)) as mult_count "
+        f"from dxlog where ContestNR = {self.pref.get('contest', '1')} "
+        "and NR GLOB '[A-Za-z][A-Za-z][0-9][0-9]';"
+    )
+    result = self.database.exec_sql(sql)
+    if result:
+        eud = result.get("mult_count", 0)
+
+    sql = (
+        "select count(DISTINCT(CountryPrefix || ':' || Mode)) as mult_count "
+        f"from dxlog where ContestNR = {self.pref.get('contest', '1')};"
+    )
+    result = self.database.exec_sql(sql)
+    if result:
+        country_prefix = result.get("mult_count", 0)
+
+    if rtc is not None:
+        return eud, country_prefix
+
+    return eud + country_prefix
+
+
+def show_qso(self):
+    """Return qso count"""
+    result = self.database.fetch_qso_count()
+    if result:
+        return int(result.get("qsos", 0))
+    return 0
+
+
+def calc_score(self):
+    """Return calculated score"""
+    # Multipliers: Each US State + DC once per mode
+    _points = get_points(self)
+    _mults = show_mults(self)
+    _power_mult = 1
+    return _points * _power_mult * _mults
+
+
+def adif(self):
+    """Call the generate ADIF function"""
+    gen_adif(self, cabrillo_name)
+
+
+def output_cabrillo_line(line_to_output, ending, file_descriptor, file_encoding):
+    """"""
+    print(
+        line_to_output.encode(file_encoding, errors="ignore").decode(),
+        end=ending,
+        file=file_descriptor,
+    )
+
+
+def cabrillo(self, file_encoding):
+    """Generates Cabrillo file. Maybe."""
+
+    now = datetime.datetime.now()
+    date_time = now.strftime("%Y-%m-%d_%H-%M-%S")
+    filename = (
+        str(Path.home())
+        + "/"
+        + f"{self.station.get('Call', '').upper().replace('/','-')}_{cabrillo_name}_{date_time}.log"
+    )
+    self.log_info(f"Saving log to:{filename}")
+    log = self.database.fetch_all_contacts_asc()
+    try:
+        with open(filename, "w", encoding=file_encoding, newline="") as file_descriptor:
+            output_cabrillo_line(
+                "START-OF-LOG: 3.0",
+                "\r\n",
+                file_descriptor,
+                file_encoding,
+            )
+            output_cabrillo_line(
+                f"CREATED-BY: Not1MM v{__version__}",
+                "\r\n",
+                file_descriptor,
+                file_encoding,
+            )
+            output_cabrillo_line(
+                f"CONTEST: {cabrillo_name}",
+                "\r\n",
+                file_descriptor,
+                file_encoding,
+            )
+            if self.station.get("Club", ""):
+                output_cabrillo_line(
+                    f"CLUB: {self.station.get('Club', '')}",
+                    "\r\n",
+                    file_descriptor,
+                    file_encoding,
+                )
+            output_cabrillo_line(
+                f"CALLSIGN: {self.station.get('Call','')}",
+                "\r\n",
+                file_descriptor,
+                file_encoding,
+            )
+            output_cabrillo_line(
+                f"LOCATION: {self.station.get('ARRLSection', '')}",
+                "\r\n",
+                file_descriptor,
+                file_encoding,
+            )
+            output_cabrillo_line(
+                f"CATEGORY-OPERATOR: {self.contest_settings.get('OperatorCategory','')}",
+                "\r\n",
+                file_descriptor,
+                file_encoding,
+            )
+            output_cabrillo_line(
+                f"CATEGORY-ASSISTED: {self.contest_settings.get('AssistedCategory','')}",
+                "\r\n",
+                file_descriptor,
+                file_encoding,
+            )
+            output_cabrillo_line(
+                f"CATEGORY-BAND: {self.contest_settings.get('BandCategory','')}",
+                "\r\n",
+                file_descriptor,
+                file_encoding,
+            )
+            mode = self.contest_settings.get("ModeCategory", "")
+            if mode in ["SSB+CW", "SSB+CW+DIGITAL"]:
+                mode = "MIXED"
+            output_cabrillo_line(
+                f"CATEGORY-MODE: {mode}",
+                "\r\n",
+                file_descriptor,
+                file_encoding,
+            )
+            output_cabrillo_line(
+                f"CATEGORY-TRANSMITTER: {self.contest_settings.get('TransmitterCategory','')}",
+                "\r\n",
+                file_descriptor,
+                file_encoding,
+            )
+            if self.contest_settings.get("OverlayCategory", "") != "N/A":
+                output_cabrillo_line(
+                    f"CATEGORY-OVERLAY: {self.contest_settings.get('OverlayCategory','')}",
+                    "\r\n",
+                    file_descriptor,
+                    file_encoding,
+                )
+            output_cabrillo_line(
+                f"GRID-LOCATOR: {self.station.get('GridSquare','')}",
+                "\r\n",
+                file_descriptor,
+                file_encoding,
+            )
+            output_cabrillo_line(
+                f"CATEGORY-POWER: {self.contest_settings.get('PowerCategory','')}",
+                "\r\n",
+                file_descriptor,
+                file_encoding,
+            )
+
+            output_cabrillo_line(
+                f"CLAIMED-SCORE: {calc_score(self)}",
+                "\r\n",
+                file_descriptor,
+                file_encoding,
+            )
+            ops = ""
+            list_of_ops = self.database.get_ops()
+            for op in list_of_ops:
+                ops += f"{op.get('Operator', '')}, "
+            if self.station.get("Call", "") not in ops:
+                ops += f"@{self.station.get('Call','')}"
+            else:
+                ops = ops.rstrip(", ")
+            output_cabrillo_line(
+                f"OPERATORS: {ops}",
+                "\r\n",
+                file_descriptor,
+                file_encoding,
+            )
+            output_cabrillo_line(
+                f"NAME: {self.station.get('Name', '')}",
+                "\r\n",
+                file_descriptor,
+                file_encoding,
+            )
+            output_cabrillo_line(
+                f"ADDRESS: {self.station.get('Street1', '')}",
+                "\r\n",
+                file_descriptor,
+                file_encoding,
+            )
+            output_cabrillo_line(
+                f"ADDRESS-CITY: {self.station.get('City', '')}",
+                "\r\n",
+                file_descriptor,
+                file_encoding,
+            )
+            output_cabrillo_line(
+                f"ADDRESS-STATE-PROVINCE: {self.station.get('State', '')}",
+                "\r\n",
+                file_descriptor,
+                file_encoding,
+            )
+            output_cabrillo_line(
+                f"ADDRESS-POSTALCODE: {self.station.get('Zip', '')}",
+                "\r\n",
+                file_descriptor,
+                file_encoding,
+            )
+            output_cabrillo_line(
+                f"ADDRESS-COUNTRY: {self.station.get('Country', '')}",
+                "\r\n",
+                file_descriptor,
+                file_encoding,
+            )
+            output_cabrillo_line(
+                f"EMAIL: {self.station.get('Email', '')}",
+                "\r\n",
+                file_descriptor,
+                file_encoding,
+            )
+            for contact in log:
+                the_date_and_time = contact.get("TS", "")
+                themode = contact.get("Mode", "")
+                if themode == "LSB" or themode == "USB":
+                    themode = "PH"
+                if themode in ("CW-U", "CW-L", "CW-R", "CWR"):
+                    themode = "CW"
+                frequency = str(round(contact.get("Freq", "0"))).rjust(5)
+
+                loggeddate = the_date_and_time[:10]
+                loggedtime = the_date_and_time[11:13] + the_date_and_time[14:16]
+                output_cabrillo_line(
+                    f"QSO: {frequency} {themode} {loggeddate} {loggedtime} "
+                    f"{contact.get('StationPrefix', '').ljust(13)} "
+                    f"{str(contact.get('SNT', '')).ljust(3)} "
+                    f"{str(contact.get('SentNr', '')).ljust(6)} "
+                    f"{contact.get('Call', '').ljust(13)} "
+                    f"{str(contact.get('RCV', '')).ljust(3)} "
+                    f"{str(contact.get('NR', '')).ljust(6)}",
+                    "\r\n",
+                    file_descriptor,
+                    file_encoding,
+                )
+            output_cabrillo_line("END-OF-LOG:", "\r\n", file_descriptor, file_encoding)
+        self.show_message_box(f"Cabrillo saved to: {filename}")
+    except IOError as ioerror:
+        self.log_info(f"Error saving log: {ioerror}")
+        return
+
+
+def recalculate_mults(self):
+    """Recalculates multipliers after change in logged qso."""
+
+
+def set_self(the_outie):
+    """..."""
+    globals()["ALTEREGO"] = the_outie
+
+
+def get_mults(self):
+    """"""
+    mults = {}
+    mults["country"], mults["state"] = show_mults(self, rtc=True)
+    return mults
+
+
+def just_points(self):
+    """"""
+    return get_points(self)
